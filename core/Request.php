c4e01e134e75a540ef05a0f37480be8b89e12c3e
Last Lantican <lastdavid.lantican@gmail.com>
2024-02-04 12:35:18 +0800

    refactor Request with error logs and restructure logic flow

--- core/Request.php
+++ core/Request.php
@@ -4,21 +4,39 @@ namespace Core;
 
 class Request
 {
-  public $URI = null;
+  public ?string $URI = null;
+  public string $METHOD = 'GET'; // set 'GET' as fallback request method
+
   public $PARAMETER = null;
-  public $METHOD = 'GET';
   public $CONTROLLER = [
-    'name' => 'application',
-    'action' => 'not_found',
+    'name' => 'application', // set 'application' fallback controller name
+    'action' => 'not_found', // set 'not_found' fallback controller action
   ];
-  public $ERRORS = [];
+  public array $ERRORS = [];
 
-  public function __construct(string $server_request_uri = '/', string $root_url = null, array $configured_routes = [])
+  protected ?string $SERVER_REQUEST_URI = null;
+  protected ?string $ROOT_URL = null;
+  protected array $CONFIGURED_ROUTES = [];
+
+  public function __construct(string $server_request_uri = null, string $root_url = null, array $configured_routes = [])
   {
-    $this->URI = $server_request_uri;
+    $this->SERVER_REQUEST_URI = $server_request_uri;
+    $this->CONFIGURED_ROUTES = $configured_routes;
+    $this->ROOT_URL = $root_url;
 
+    // create custom request uri
+    $this->create_URI($root_url);
+    // configure the Request with request method, parameter, and controller info based on configured routes
+    $this->configure_request($this->CONFIGURED_ROUTES);
+  }
+
+  protected function create_URI(string $root_url)
+  {
     // validate root_url as a valid url
     if ($root_url === null || !is_string($root_url) || !filter_var($root_url, FILTER_VALIDATE_URL)) {
+      $this->ERRORS[] = "Invalid 'root_url' provided: $root_url";
+
+      // set fallback url
       $root_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
     }
 
@@ -28,14 +46,15 @@ class Request
 
     if (strpos($_SERVER['REQUEST_URI'], $request_path) === 0) {
       $this->URI = substr($_SERVER['REQUEST_URI'], strlen($request_path));
+    } else {
+      $this->URI = $request_path;
     }
-
-    // process Request
-    $this->initialize($configured_routes);
   }
 
-  protected function initialize(array $configured_routes = [])
+  protected function configure_request(array $configured_routes = [])
   {
+    $match_found = false;
+
     foreach ($configured_routes as $route => $methods) {
       foreach ($methods as $method => $controller_name_action_pair) {
         // create a regular expression pattern to match the url.
@@ -44,7 +63,7 @@ class Request
 
         // check if the current request URI matches the route pattern
         // and if the HTTP method matches the specified method.
-        if ($_SERVER['REQUEST_METHOD'] === $method && preg_match($route_pattern, $this->URI, $matches)) {
+        if (preg_match($route_pattern, $this->URI, $matches) && $_SERVER['REQUEST_METHOD'] === $method) {
           // set Request parameter
           $this->PARAMETER = $matches[1] ?? null;
           // set Request method
@@ -54,8 +73,16 @@ class Request
           // set Request controller
           $this->CONTROLLER['name'] = $controller_name;
           $this->CONTROLLER['action'] = $controller_action;
+
+          $match_found = true;
+          break;
         }
       }
+    }
+
+    // if no match found, add error to Request
+    if (!$match_found) {
+      $this->ERRORS[] = "No matching route for {$_SERVER['REQUEST_METHOD']} {$this->URI}";
     }
   }
 }
