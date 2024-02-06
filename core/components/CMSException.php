8956b9982d7a68d8b0418f11a67374da4ee2eace
Last Lantican <lastdavid.lantican@gmail.com>
2024-02-06 22:59:52 +0800

    fix error handling DOM formatting

--- core/components/CMSException.php
+++ core/components/CMSException.php
@@ -6,7 +6,7 @@ class CMSException extends \Exception
 {
   public function __construct($message = "", $code = 0, \Throwable $previous = null)
   {
-    $message .= nl2br(PHP_EOL) . nl2br(PHP_EOL . "{$this->file}:{$this->line}");
+    $message .= "<p style=\"font-size: 0.75rem; text-align: right; margin-block-start: 4em;\">{$this->file}:{$this->line}</p>";
     parent::__construct($message, $code, $previous);
   }
 }
--- core/traits/ErrorHandler.php
+++ core/traits/ErrorHandler.php
@@ -2,13 +2,15 @@
 
 namespace Core\Traits;
 
+use Core\Components\CMSException;
+
 trait ErrorHandler
 {
   public function run_error_check()
   {
     if (!empty($this->ERRORS)) {
       // throw an exception to halt the execution
-      throw new \Exception();
+      throw new CMSException();
     }
   }
 
@@ -16,7 +18,16 @@ trait ErrorHandler
   {
     $number_of_errors = count($this->ERRORS) > 1 ? 'Errors' : 'Error';
 
-    $html = '<div style="width: min(900px, 100% - 8em); padding: 2em; margin-inline: auto; margin-block-start: 2em; background-color: hsl(0,60%,96%,1); border-radius: 0.5em 0.5em 0em 0em; border-top: 4px solid maroon; box-shadow: 1px 1px 1px 1px hsl(0,0%,0%,0.1);">';
+    $html = '<!DOCTYPE html>';
+    $html .= '<html lang="en">';
+    $html .= '<head>';
+    $html .= '<meta charset="UTF-8">';
+    $html .= '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    $html .= "<title>{$number_of_errors} Found!</title>";
+    $html .= '</head>';
+    $html .= '<body style="font-family: sans-serif;">';
+
+    $html .= '<div style="width: min(900px, 100% - 8em); padding: 2em; margin-inline: auto; margin-block-start: 2em; background-color: hsl(0,60%,96%,1); border-radius: 0.5em 0.5em 0em 0em; border-top: 4px solid maroon; box-shadow: 1px 1px 1px 1px hsl(0,0%,0%,0.1);">';
     $html .= "<h2>{$number_of_errors} found:</h2>";
     $html .= '<ul>';
 
@@ -27,8 +38,12 @@ trait ErrorHandler
     $html .= '</ul>';
     $html .= '<hr style="margin-block: 2em; border: none; border-top: 2px dotted gray;">';
     $html .= '<p>' . "{$number_of_errors} occurred during execution. Halting further execution." . '</p>';
-    $html .= $exception->getMessage();
+    $html .= "<p style=\"word-break: break-all;\">" . $exception->getMessage() . "</p>";
     $html .= '</div>';
+
+    $html .= '</body>';
+    $html .= '</html>';
+
 
     // display the HTML or log it, based on your requirements
     echo $html;
